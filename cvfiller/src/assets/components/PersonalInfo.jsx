b39389c07fcc211cb39b3650b041ed74e59b209a
GiroFrm <girofram844@gmail.com>
2024-05-31 20:47:34 +1000

    feat: add form to PersonalInfo component

--- cvfiller/src/assets/components/PersonalInfo.jsx
+++ cvfiller/src/assets/components/PersonalInfo.jsx
@@ -3,16 +3,33 @@ import { useState } from "react";
 
 function PersonalInfo() {
  
+    const [name, setName] = useState('');
+    const [email, setEmail]= useState('');
+    const [number, setNumber]= useState('');
+    
+    function submit(event){
+        event.preventDefault();
+        alert(name)
+    }
+
     return(
         <>
         <h1>Personal Info</h1> 
-        <form action="">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name"/><br/>
+        <form onSubmit={submit}>
+          <label >Name</label>
+          <input type="text"
+          value={name} 
+          onChange={e=> setName(e.target.value)}/><br/>
           <label htmlFor="email">email</label> 
-          <input type="email" id="email"/> <br/>
+          <input type="email" id="email" 
+          value={email}
+          onChange={e=>setEmail(e.target.value)}
+          /> <br/>
           <label htmlFor="number">Number</label>
-          <input type="tel" id="number"/>
+          <input type="tel" id="number" value={number}
+           onChange={e=>setNumber(e.target.value)}/> <br/>
+         <input type="submit"/>
+        
         </form>
         </>
     )
