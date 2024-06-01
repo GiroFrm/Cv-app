cc9f070a3069e7a6d2f5b1691b0e81760069eef3
GiroFrm <girofram844@gmail.com>
2024-06-01 19:55:14 +1000

    refactor: consolidate useState hooks into a single state object
    combined school title startDate endDate  state variables into a single inputs object

--- cvfiller/src/assets/components/Education.jsx
+++ cvfiller/src/assets/components/Education.jsx
@@ -3,18 +3,20 @@ import { useLayoutEffect, useState } from "react";
 
 function Education() {
    
-    const [school, setSchool]= useState('');
-    const [title, setTitle]=useState('');
-    const[startDate, setStartDate] = useState('');
-    const[endDate, setEndDate] = useState('');
+
+    const [inputs, setInputs]=useState({});
 
    const [isSubmit, setIsSubmit]= useState(false);
    
-
+   function handleChange(event){
+    const name = event.target.name;
+    const value= event.target.value;
+    setInputs(values=>({...values, [name]:value}))
+}
     function submit(event){
         event.preventDefault()
-        if(school!=''&& title!=''
-        &&startDate!=''&&endDate!=''){
+        if(inputs.school!=''&& inputs.title!=''
+        &&inputs.startDate!=''&&inputs.endDate!=''){
         setIsSubmit(!isSubmit)
         }
     }
@@ -25,22 +27,22 @@ function Education() {
         { ! isSubmit? (
          <form onSubmit={submit}>
           <label htmlFor="" >School name</label>
-          <input type="text" value={school} onChange={e=>{setSchool(e.target.value)}}/> <br/>
+          <input type="text" name="school" value={inputs.school} onChange={handleChange}/> <br/>
           <label htmlFor="">Title</label>
-          <input type="text" value={title} onChange={e=>{setTitle(e.target.value)}}/> <br/>
+          <input type="text" name="title" value={inputs.title}  onChange={handleChange}/> <br/>
           <label htmlFor="">Start Date</label>
-          <input type="date" value={startDate} onChange={e=>{setStartDate(e.target.value)}}/> <br/>
+          <input type="date" name="startDate" value={inputs.startDate} onChange={handleChange}/> <br/>
           <label htmlFor="">End Date</label>
-          <input type="date" value={endDate} onChange={e=>{setEndDate(e.target.value)}}/> <br/>
+          <input type="date" name="endDate" value={inputs.endDate} onChange={handleChange}/><br/>
           <input type="submit" />
          </form>):
          (
          <div>
          <ul>
-           <li><p>School name</p><p>{school}</p></li>
-           <li><p>Title</p><p>{title}</p></li>
-           <li><p>Start Date</p><p>{startDate}</p></li>
-           <li><p>End Date</p><p>{endDate}</p></li>
+           <li><p>School name</p><p>{inputs.school}</p></li>
+           <li><p>Title</p><p>{inputs.title}</p></li>
+           <li><p>Start Date</p><p>{inputs.startDate}</p></li>
+           <li><p>End Date</p><p>{inputs.endDate}</p></li>
          </ul>
          <button onClick={submit}>Edit</button>
          </div>
